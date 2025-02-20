6e931b98162ce0b45704a2a83bceeb04cb712bab
bellawuzhang <bella.wu.zhang@gmail.com>
2025-02-20 13:51:11 +0000

    finished home page and nav

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -2,4 +2,14 @@ class DiceController < ApplicationController
   def two_six
     render({ :template => "dice_templates/2d6"})
   end
+
+  def two_ten
+    render({ :template => "dice_templates/2d10"})
+  end
+
+
+  def homepage
+    render({ :template => "dice_templates/homepage"})
+  end
+
 end
