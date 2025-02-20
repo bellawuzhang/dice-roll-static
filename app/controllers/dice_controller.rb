3235602fa7d3a18bce47404c64b5ccef3c04f49c
bellawuzhang <bella.wu.zhang@gmail.com>
2025-02-20 13:58:56 +0000

    final changes

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -12,4 +12,13 @@ class DiceController < ApplicationController
     render({ :template => "dice_templates/homepage"})
   end
 
+  def one_twenty
+    render({ :template => "dice_templates/1d20"})
+  end
+
+  def five_four
+  
+    render({ :template => "dice_templates/5d4"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,8 @@ Rails.application.routes.draw do
   get("/dice/2/10", { :controller => "dice", :action => "two_ten" })
 
   get("/", { :controller => "dice", :action => "homepage" })
+
+  get("/dice/1/20", { :controller => "dice", :action => "one_twenty" })
+
+  get("/dice/5/4", { :controller => "dice", :action => "five_four" })
 end
